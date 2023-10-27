eacbb3b533c377c9230bd415d374351546b95e86
justacowboyy <145454504+justacowboyy@users.noreply.github.com>
2023-10-27 18:54:16 +0530

    Update hashmapfunctions.java

--- hashmapfunctions.java
+++ hashmapfunctions.java
@@ -43,3 +43,70 @@ public class HashMapExample {
         System.out.println("Is empty? " + hashMap.isEmpty());
     }
 }
+
+
+
+
+
+and
+
+
+
+
+    
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapFunctions {
+    public static void main(String[] args) {
+        // Creating a HashMap
+        Map<Integer, String> hashMap = new HashMap<>();
+
+        // Insert key-value pairs
+        hashMap.put(1, "One");
+        hashMap.put(2, "Two");
+        hashMap.put(3, "Three");
+
+        // Checking if a value exists
+        boolean containsValue = hashMap.containsValue("Two");
+        System.out.println("Contains 'Two': " + containsValue); // Output: true
+
+        // Getting the set of key-value pairs
+        for (Map.Entry<Integer, String> entry : hashMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
+        // Getting keys as a set
+        for (Integer key : hashMap.keySet()) {
+            System.out.println("Key: " + key);
+        }
+
+        // Getting values as a collection
+        for (String value : hashMap.values()) {
+            System.out.println("Value: " + value);
+        }
+
+        // Replace the value associated with a key
+        hashMap.replace(2, "NewTwo");
+        System.out.println("Updated value for key 2: " + hashMap.get(2)); // Output: NewTwo
+
+        // Compute a value based on the key (if absent)
+        hashMap.computeIfAbsent(4, key -> "Four");
+        System.out.println("Value for key 4: " + hashMap.get(4)); // Output: Four
+
+        // Merge values for a given key
+        hashMap.merge(3, "Three", (existingValue, newValue) -> existingValue + newValue);
+        System.out.println("Merged value for key 3: " + hashMap.get(3)); // Output: ThreeThree
+
+        // Convert the hashmap to a string
+        String hashMapToString = hashMap.toString();
+        System.out.println("HashMap as a string: " + hashMapToString);
+
+        // Copy the content of one hashmap to another
+        Map<Integer, String> copyHashMap = new HashMap<>(hashMap);
+
+        // Check for equality with another hashmap
+        boolean areEqual = hashMap.equals(copyHashMap);
+        System.out.println("Are hashmaps equal? " + areEqual); // Output: true
+    }
+}
